737cf87adc4f2a8262f468c7b003b61c38243b67
RLant <rebeccalant@gmail.com>
2021-03-18 17:41:37 -0400

    adding JS functionality

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const list = document.getElementById("list");
+const text = document.getElementById("text");
+const addBtn = document.getElementById("addBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+const checkBtn = document.getElementById("checkBtn");
+const form = document.getElementById("form");
+
+
+
+//adds item typed in to the list(the DOM). 
+function addItemToDOM() {
+    if (text.value.trim() === "") {
+        alert("please add a grocery item")
+    } else {
+        //create a variable for the input
+        let input = text.value;
+        //create a new li element. 
+        let newLi = document.createElement("li");
+        //specify what the innerHTML is going to be inside the new li element
+        let addedItem = newLi.innerHTML = `<button class="deleteBtn" id="deleteBtn">X</button><button class="checkBtn" id="checkBtn">✔</button>•${input}`;
+        //new li inner html
+        newLi.innerHTML = addedItem;
+        //add the li element to the list (DOM). 
+        list.appendChild(newLi);
+        text.value = "";
+        console.log(this.parentNode);
+        console.log(this.deleteBtn.parentNode);
+        deleteBtn.addEventListener("click", deleteItem); 
+    }
+}
+
+function deleteItem(){
+    console.log(this.deleteBtn.parentNode);
+}
+
+
+//Event Listeners
+addBtn.addEventListener("click", addItemToDOM);
+
+ 
